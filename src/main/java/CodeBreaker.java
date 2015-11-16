6c8cdb6c4a3cba9892c4b351e7fbd9a3c4904936
mateopineda <jasonpineda100@gmail.com>
2015-11-16 11:30:39 -0500

    Presentes y correctos

--- src/main/java/CodeBreaker.java
+++ src/main/java/CodeBreaker.java
@@ -2,17 +2,46 @@
  * Created by alexis on 7/11/15.
  */
 public class CodeBreaker {
-    public String compare(int pensado,int original){
-        String pensadoString = String.valueOf(pensado);
-        String originalString = String.valueOf(original);
-
+    public String compare(String pensado,String original){
+        String retorno="";
         if( pensado == original) {
-            return "XXXX";
+            retorno= "X X X X";
         }
-        else if (pensadoString.charAt(0) == originalString.charAt(0)){
-            return "X";
+        else{
+            if (pensado.charAt(0) == original.charAt(0)){
+                retorno += "X ";
+            } else if(pensado.contains(original.substring(0,1))) {
+                retorno += "_ ";
+            }
+            else{
+                retorno += "  ";
+            }
+            if (pensado.charAt(1) == original.charAt(1)){
+                retorno += "X ";
+            } else if(pensado.contains(original.substring(1,2))) {
+                retorno += "_ ";
+            }
+            else{
+                retorno += "  ";
+            }
+            if (pensado.charAt(2) == original.charAt(2)){
+                retorno += "X ";
+            } else if(pensado.contains(original.substring(2,3))) {
+                retorno += "_ ";
+            }
+            else{
+                retorno += "  ";
+            }
+            if (pensado.charAt(3) == original.charAt(3)){
+                retorno += "X";
+            } else if(pensado.contains(original.substring(3,4))) {
+                retorno += "_";
+            }
+            else{
+                retorno += " ";
+            }
         }
-        return "";
 
+        return retorno;
     }
 }
